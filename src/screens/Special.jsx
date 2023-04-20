39ac36752d228752473797f489764a02a8080738
iDK-pseudo <shivamsharma151999@gmail.com>
2023-04-20 12:47:05 +0530

    Special screen scroll fix

--- src/screens/Special.jsx
+++ src/screens/Special.jsx
@@ -1,19 +1,16 @@
 import { Banner } from "../components/Banner.jsx";
 import { FoodCard } from "../components/FoodCard.jsx";
 import { useDispatch, useSelector } from 'react-redux';
+import "../styles/SpecialScreen.css"
 
 export function Special () {
     
   const allItems = useSelector(state => state.foodRed.allItems);
 
     return (
-        <div>
+        <div className="special-container">
             <Banner imgSrc="https://toast-prod-data.s3.ap-south-1.amazonaws.com/restruant/52553522735652184/2020-06-20/feature_image_2.jpeg"/>
-            <div style={{
-                display: "flex",
-                flexWrap: "wrap",
-                width: "100%"
-            }}>
+            <div className="special-food-card-container">
                 {allItems.map((item)=><FoodCard item={item}/>)}
             </div>
         </div>
